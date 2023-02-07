fa08278d01db1276a3ecbf2ae41cc812b16b599d
Marco <104728524+MarcoEAEsteban@users.noreply.github.com>
2023-02-07 17:59:27 -0300

    opciones de frutas

--- ejercicio9/index.js
+++ ejercicio9/index.js
@@ -13,3 +13,30 @@ function verificar(){
         res.innerHTML += genero
     }
 }
+
+
+let bot = document.getElementById('agregar')
+bot.addEventListener('click', agregar)
+
+function agregar(){
+    let respuesta = document.getElementById('respuesta')
+    let frutas = document.getElementsByName('frutas')
+    let tipos = ''
+
+    if(frutas[0].checked){
+        tipos = 'Pera'
+        respuesta.innerHTML += tipos 
+    }else if(frutas[1].checked){
+            tipos = 'Manzana'
+            respuesta.innerHTML += tipos
+    }else if(frutas[2].checked){
+            tipos = 'Patano'
+            respuesta.innerHTML += tipos
+    }else if(frutas[3].checked){
+            tipos = 'Uva'
+            respuesta.innerHTML += tipos
+    }else{
+
+    }
+    
+}
