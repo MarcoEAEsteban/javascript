c8aa0f32721e748160ec42271106f1c6cd09b9c7
Marco <104728524+MarcoEAEsteban@users.noreply.github.com>
2023-02-11 18:31:17 -0300

    practica

---
+++ aula15/index.js
@@ -0,0 +1,12 @@
+let palNueva = document.getElementById('agregar')
+let lista = []
+let boton = document.getElementById('botagregar')
+
+boton.addEventListener('click', colocar)
+function colocar(){
+    let numeros = document.getElementById('palabras')
+    numeros.innerHTML += palNueva.value
+    lista.push(palNueva.value)
+    console.log(lista)
+    formulario.reset()
+}
