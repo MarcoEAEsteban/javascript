5643f4d0569c1a2ba840d42b50689187c80a39d1
Marco Aucaruri <104728524+MarcoEAEsteban@users.noreply.github.com>
2023-04-15 19:41:35 -0300

    h

--- ejercicio18 logica programacion/cadenaTexto.js
+++ ejercicio18 logica programacion/cadenaTexto.js
@@ -118,4 +118,107 @@ const capicua = (numero=0) =>{
         ?console.info(`Si es capicua, Numero original:${numero}, Numero al reves: ${alReves}`)
         :console.info(`No es capicua, numero original: ${numero}, Numero al reves: ${alReves}`)
 }
-capicua(202)
+//capicua(212.212)
+
+
+//Programa una función que calcule el factorial de un número(El factorial de un entero positivo 'n', se define como el producto de todos los números enteros positivos del 1 hasta 'n'), pe.miFuncion(5) devolvera:120
+const miFactrial = (numero = undefined) =>{
+    if(numero === undefined) return console.warn('digite un numero')
+    if(typeof numero !== 'number') return console.error('no es un numero')
+    if(numero === 0) return console.info('El número no puede ser cero')
+    if(Math.sign(numero)===-1) return console.error('El numero no puede ser negativo')
+
+    let factorial = 1
+    for( let i = numero; i>1; i--){
+        factorial *= i
+    }
+    return console.info(`El factorial de ${numero} es: ${factorial}`)
+}
+//miFactrial(5)
+
+//programa una funcion que te determine si un número es primo (aquel que solo es divisible por si mismo y 1) o no, pe.miFuncion(7) devolvera true  
+const numeroPrimo = (numero = undefined) =>{
+    if(numero === undefined) return console.warn('Digite un numero')
+    if(typeof numero !== 'number') return console.error('no es un numero')
+    if(numero === 0) return console.error('el numero no puede ser cero')
+    if(numero === 1) return console.error('el numero no puede ser 1')
+    if(Math.sign(numero)===-1) return console.error('el numero no puede ser negativo')
+    let divisible = false
+    for(let i = 2; i < numero; i++){
+        if((numero % i)===0){
+            divisible = true
+        }
+    }
+
+    return (divisible)
+        ?console.log(`El numero ${numero}, no es primo`)
+        :console.log(`el numero ${numero}, si es primo`)
+}
+//numeroPrimo(4)
+
+// Programa una funcion que determine si un numero es par o impar, pe.miFuncion(29) devolvera impar
+
+const numeroPar = (numero = undefined)=>{
+    if(numero === undefined) return console.warn('No ingresaste un numero')
+    if(typeof numero !== 'number') return console.error(`El valor: ${numero}, no es un numero`)
+
+    return ((numero%2)===0)
+        ?console.info(`el numero ${numero}, es par`)
+        :console.info(`El numero ${numero}, es impar`)
+}
+//numeroPar(12)
+
+//programa una funcion para convertir grado celsius a fahrenheit y viceversa, pe.miFuncion(0,'°C') devolvera 32°F
+const convertirGrados = (grados = undefined,unidad = undefined)=>{
+    if(grados===undefined) return console.warn('no ingresaste grados a convertir')
+    if(typeof grados!=='number') return console.error(`el valor ${grados} ingresado no es un numero`)
+    if(unidad===undefined) return console.warn('no ingresaste la unidad a convertir')
+    if(typeof unidad!=='string') return console.error(`el valor ${unidad} ingresado no es un numero`)
+
+    if(unidad.length !== 1 || !/(C|F)/.test(unidad)) return console.warn('valor de la unidad no reconocido')
+
+    if(unidad === 'C'){
+        return console.info(`${grados}°C= ${Math.round((grados*(9/5))+32)}°F`)
+    }else if(unidad === 'F'){
+        return console.info(`${grados}°C= ${Math.round((grados - 32)*(5/9))}°C`)
+    }
+}
+//convertirGrados(100, 'C')
+//convertirGrados(212, 'F')
+
+//programa una funcion para convertir numeros de base binaria a decimal y viceversa,  pe.miFuncion(100, 2) devolvera 4 base 10
+const convertirBinarioDecimal = (numero = undefined, base = undefined)=>{
+    if(numero === undefined) return console.warn('no ingresaste el numero a convertir')
+    if(typeof numero !== 'number') return console.error(`El valor "${numero}" ingresado, NO es un número`)
+
+    if(base === undefined) return console.warn('no ingresaste el numero a convertir')
+    if(typeof base !== 'number') return console.error(`El valor "${base}" ingresado, NO es un número`)
+
+    if(base === 2){
+        return console.info(`${numero} base ${base} = ${parseInt(numero, base)} base 10`)
+    }else if(base === 10){
+        return console.info(`${numero} base${base} = ${(numero.toString(base))} base 2`)
+    }else{
+        return console.error('El tipo de base a convertir NO es valido')
+    }
+
+} 
+//convertirBinarioDecimal(111001010,2)
+//convertirBinarioDecimal(458,10)
+
+
+//Programa una funcion que devuelva el monto final despues de aplicar un descuaento a una cantidad dada, pe.miFuncion(100,20) devolvera 800
+const aplicarDescuento = (monto = undefined, descuento = 0)=>{
+    if(monto === undefined) return console.warn('no ingresaste el monto')
+    if(typeof monto !== 'number') return console.error(`El valor "${monto}" ingresado,NO es un numero`)
+    if(monto === 0) return console.error('El monto no puede ser 0')
+    if(Math.sign(monto)=== -1) return console.error('El monto no puede ser negativo')
+    if(typeof descuento !== 'number') return console.error(`El valor "${descuento}" ingresado,NO es un numero`)
+    if(Math.sign(descuento)=== -1) return console.error('El descuento no puede ser negativo')
+
+    return console.info(`${monto}-${descuento}% = ${monto-((monto*descuento)/100)}`)
+}
+//aplicarDescuento(100,50)
+
+
+//programa una funcion que dada una fecha determine cuantos años han pasado hasta el dia de hoy, pe.miFuncion(new date(1984,4,23))
